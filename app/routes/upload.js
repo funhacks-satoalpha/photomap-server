a682bc4ca60324b9d58c143496f44a503c0c975d
taross <taross14.selkie@gmail.com>
2016-12-17 11:42:00 +0000

    [add]project files

---
+++ app/routes/upload.js
@@ -0,0 +1,13 @@
+var express = require("express");
+var router = express.Router();
+
+var fs = require("fs");
+var firebase = require("firebase");
+
+
+router.post("/", function (req,res,next) {
+
+});
+
+
+
---
+++ provision.sh
@@ -0,0 +1,6 @@
+sudo apt-get install -y git
+sudo apt-get install -y nodejs npm
+sudo npm cache clean
+sudo npm install n -g
+sudo n stable
+sudo ln -sf /usr/local/bin/node usr/bin/node
